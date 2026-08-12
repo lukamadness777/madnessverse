632190524a58afbfa1d84d52f64c3bff7be05512
lukamadness777 <joaquinlima172008@gmail.com>
2026-08-12 17:46:23 -0300

    Initial commit

---
+++ common/src/main/java/dev/lukamadness/ExampleMod.java
@@ -0,0 +1,9 @@
+package dev.lukamadness;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "madnessverse";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
